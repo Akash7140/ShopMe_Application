268d8bf46654e6e73659295faf6bdb4326713f6b
Akash7140 <akashkrsingh7140@gmail.com>
2023-07-12 02:22:54 +0530

    Added password encryption using BCrypt

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/PasswordEncoderTest.java
@@ -0,0 +1,19 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTest {
+	
+	@Test
+	public void testEncodePassword() {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String rawPassword = "dummyPassword123";
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+		//System.out.println(encodedPassword);
+		boolean match = passwordEncoder.matches(rawPassword, encodedPassword);
+		assertThat(match).isTrue();
+	}
+}
